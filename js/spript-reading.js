f963ccfde9f0b2c54b20843a69335b7aa0d8babe
Darja <darya.the.teacher@gmail.com>
2025-10-01 15:46:40 +0400

    Update spript-reading.js
    commets deleted

--- js/spript-reading.js
+++ js/spript-reading.js
@@ -2,12 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const gapSelects = document.querySelectorAll('.gap-select');
 
     gapSelects.forEach(select => {
-        // Изначально проверяем все селекты
         if (select.value === '') {
             select.classList.add('is-empty');
         }
 
-        // Добавляем обработчик события 'change'
         select.addEventListener('change', () => {
             if (select.value === '') {
                 select.classList.add('is-empty');
